e6897c3527b8e6858cd7aeab4163d71b9f8271f8
NaEun Kim <121096778+hellonaeunkim@users.noreply.github.com>
2023-07-31 18:13:51 -0700

    Add files via upload

--- project_9_higher_lower/main.py
+++ project_9_higher_lower/main.py
@@ -1,1 +1,58 @@
+from game_data import data
+import random
+from art import logo, vs
+from replit import clear
 
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
+
+def information(account):
+  """Get the information in data dictionary"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  return f"{name}, a {description}, from {country}"
+
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+
+def game():
+  print(logo)
+  score = 0
+  game_continue = True
+  account_b = get_random_account()
+
+  while game_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {information(account_a)}.")
+    print(vs)
+    print(f"Against B: {information(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    clear()
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+game()
