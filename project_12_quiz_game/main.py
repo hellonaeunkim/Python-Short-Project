be61896ab4405d449522c6435b09561cadc01cc7
NaEun Kim <121096778+hellonaeunkim@users.noreply.github.com>
2023-07-31 18:22:48 -0700

    Add files via upload

--- project_12_quiz_game/main.py
+++ project_12_quiz_game/main.py
@@ -1,1 +1,19 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizeBrain
 
+question_bank = []
+for data in question_data:
+  question_text = data["text"]
+  question_answer = data["answer"]
+  new_question = Question(question_text, question_answer)
+  question_bank.append(new_question)
+
+
+quiz = QuizeBrain(question_bank)
+
+while quiz.still_has_questions():
+  quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score is {quiz.score}/{quiz.question_number}.")
