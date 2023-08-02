4f91188e8095bd33fc3325136ae50934b94a4aff
NaEun Kim <121096778+hellonaeunkim@users.noreply.github.com>
2023-08-01 20:03:51 -0700

    Add files via upload

--- project_021_squirrel_data_analysis/main.py
+++ project_021_squirrel_data_analysis/main.py
@@ -1,1 +1,18 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+gray_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrel_count, cinnamon_squirrel_count, black_squirrel_count]
+}
+
+df = pandas.DataFrame(data_dict)
+
+df.to_csv("squirrel_count.csv")
 
