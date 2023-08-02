98a6c33c35e07d3dcf31d9c73839d6ced0b359e8
NaEun Kim <121096778+hellonaeunkim@users.noreply.github.com>
2023-08-01 20:07:45 -0700

    Add files via upload

--- project_023_nato_alphabet/main.py
+++ project_023_nato_alphabet/main.py
@@ -1,1 +1,8 @@
+import pandas
 
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+name = input("Enter your name : ").upper()
+output_list = [nato_dict[word] for word in name]
+print(output_list)
